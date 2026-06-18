a74d4f8ba9b1d391344c2a6c5a460588fc4ca225
Ram Meena <YOUR_GITHUB_EMAIL>
2026-06-18 13:08:08 +0530

    Fix CORS

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // MongoDB Connect
 mongoose
